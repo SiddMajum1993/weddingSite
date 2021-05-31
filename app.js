0c6679853f2f50d0900a5678aa78052697a5a7b5
Siddhartha Majumdar <siddhartha.majumdar@salesforce.com>
2021-05-31 23:04:42 +0530

    Added changes and commented that

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.to(".hero-block_title", {
   y: 100,
   duration: 3,
@@ -7,15 +9,6 @@ gsap.to(".hero-block_title", {
     start: "top center",
   },
 });
-
-// gsap.to(".hero-block_title", {
-//   y: 200,
-//   duration: 3,
-//   scrollTrigger: {
-//     trigger: ".hero-block_title",
-//     start: "top top+=100px",
-//   },
-// });
 
 gsap.to(".menu", {
   duration: 3,
@@ -36,3 +29,50 @@ gsap.to(".header-block_logo", {
     horizontal: true,
   },
 });
+
+const tl = gsap.timeline();
+tl.from(".block-one_image", {
+  duration:2,
+  xPercent: -100,
+}).from(".block-two_image",{
+  duration:2,
+  yPercent: 50,
+});
+
+
+// const sections = gsap.utils.toArray(".story-container");
+// let maxWidth = 0;
+
+// const getMaxWidth = () => {
+//   maxWidth = 0;
+//   sections.forEach((section) => {
+//     maxWidth += section.offsetWidth;
+//   });
+// };
+
+// getMaxWidth();
+// ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+// gsap.to(sections, {
+//   x: () => `-${maxWidth - window.innerWidth}`,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".story-timeline_wrapper",
+//     pin: true,
+//     scrub: true,
+//     end: () => `+=${maxWidth}`,
+//     invalidateOnRefresh: true
+//   }
+// });
+
+// sections.forEach((sct, i) => {
+//   ScrollTrigger.create({
+//     trigger: sct,
+//     start: () =>
+//       "top top-=" +
+//       (sct.offsetLeft - window.innerWidth / 2) *
+//         (maxWidth / (maxWidth - window.innerWidth)),
+//     end: () =>
+//       "+=" + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth))
+//   });
+// });
